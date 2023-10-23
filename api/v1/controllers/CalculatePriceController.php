f5e21a9f5f17d95e84e3422c95056417b3428e50
cr-ovr6 <cr-ovr6@efko.ru>
2023-10-23 16:35:38 +0300

    доделано + рефакторинг

--- api/v1/controllers/CalculatePriceController.php
+++ api/v1/controllers/CalculatePriceController.php
@@ -3,34 +3,28 @@
 namespace api\v1\controllers;
 
 use Yii;
-use yii\helpers\ArrayHelper;
 use yii\rest\Controller;
-
+use api\v1\models\Price;
 
 class CalculatePriceController extends Controller
 {
-
-
-    public function actionIndex(){
+    public function actionIndex()
+    {
         $request = Yii::$app->request;
         $material = $request->get('material');
         $weight = $request->get('weight');
         $month = $request->get('month');
-        if (empty($material)){
+        if (empty($material)) {
             return 'Ошибка! Сырье не введено';
-            die();
         }
-        if (empty($weight)){
+        if (empty($weight)) {
             return 'Ошибка! Вес не введен';
-            die();
         }
-        if (empty($month)){
+        if (empty($month)) {
             return 'Ошибка! Месяц не введен';
-            die();
         }
-        $arrays = Yii::$app->params['arrays'];
-        $array = ArrayHelper::getValue($arrays, $material);
-        $calculation = $array[$month][$weight];
-        return array($calculation,$array);
+        $resp = new Price();
+        $resp->response($material, $month, $weight);
+        return $resp;
     }
 }
